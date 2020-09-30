a7a6998cbfd092aaa1a353c815d110c0be4e1770
Adam <adam.lance.steinberg@gmail.com>
2020-09-30 19:55:33 -0400

    progress

---
+++ Circle.h
@@ -0,0 +1,9 @@
+#pragma once
+//functions for creating a circle
+#include <vector>
+#include "Vec2.h"
+
+class Circle : Polygon //a Circle is a Polygon with many sides, so it inherits
+{
+  Circle(int radius, Vec2 center)
+};
